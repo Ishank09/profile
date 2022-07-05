edd735f739e72bde8d2f82ebef273ea4e6b6d524
Ishank_Vasania <Ishank_Vasania@Dell.com>
2022-07-05 17:58:45 +0530

    Refresh working, 404 NF error fixed

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { useContext } from 'react';
-import { BrowserRouter as Router, Route, Switch, Redirect } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Switch, Redirect, HashRouter } from 'react-router-dom';
 
 import { ThemeContext } from './contexts/ThemeContext';
 import { Main, BlogPage, ProjectPage, CertificatePage, AchievementPage } from './pages'
@@ -19,14 +19,17 @@ function App() {
     <div className="app">
       <Router>
         <ScrollToTop/>
-        <Switch>
+        <HashRouter basename="/">
+          <Switch>
           <Route path="/" exact component={Main} />
           <Route path="/blog" exact component={BlogPage} />
           <Route path="/projects" exact component={ProjectPage} />
           <Route path="/certificate" exact component={CertificatePage} />
           <Route path="/achievement" exact component={AchievementPage} />
           <Redirect to="/" />
-        </Switch>
+          </Switch>
+          
+        </HashRouter>
       </Router>
       <BackToTop />
     </div>
--- src/index.js
+++ src/index.js
@@ -5,13 +5,18 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 import ThemeContextProvider from './contexts/ThemeContext'
 
-
+import { BrowserRouter} from 'react-router-dom';
 import "slick-carousel/slick/slick.css"; 
 import "slick-carousel/slick/slick-theme.css";
 
 ReactDOM.render(
     <ThemeContextProvider>
+           {/* <React.StrictMode>
+    <BrowserRouter> */}
       <App />
+    {/* </BrowserRouter>
+  </React.StrictMode>, */}
+     
     </ThemeContextProvider>,
   document.getElementById('root')
 );
